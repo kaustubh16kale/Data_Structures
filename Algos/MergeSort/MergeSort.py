1e7130f832d024b3ff0a4e0500f6e041ab78051f
Kaustubh Kale <kaustubh.mum.dbda@gmail.com>
2024-05-08 14:10:22 +0530

    [Kaustubh Kale] Add: Merge sort algorithm

---
+++ Algos/MergeSort/MergeSort.py
@@ -0,0 +1,49 @@
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    L = [0] * n1
+    R = [0] * n2
+
+    for i in range(n1):
+        L[i] = arr[l + i]
+    for j in range(n2):
+        R[j] = arr[m + 1 + j]
+
+    i = 0  # index of first arr
+    j = 0  # index of second arr
+    k = l  # index of merged subarray
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def merge_sort(arr, l, r):
+    if l < r:
+        m = (l + r) // 2
+        merge_sort(arr, l, m)
+        merge_sort(arr, m + 1, r)
+        merge(arr, l, m, r)
+
+arr = [1, 100, 12, 4, 58, 6, 25, 632, 155, 458, 8, 0, 12, 3, 5]
+n = len(arr)
+print("Given array is:", arr)
+
+merge_sort(arr, 0, n - 1)
+
+print("Sorted array is:", arr)
